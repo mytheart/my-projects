77bacb66e0eb961ce5cc48ea89b059c0e6cacf22
张印 <550703617@qq.com>
2018-11-27 01:08:28 +0800

    增加了ajax方法

--- mine.js
+++ mine.js
@@ -547,3 +547,34 @@ function traverse(element, callback) {
         traverse(list[i], callback);
     }
 }
+
+// 34.原生js封装ajax
+function ajax(method, url, callback, data, flag) {
+    var xhr;
+    flag = flag || true;
+    method = method.toUpperCase();
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject('Microsoft.XMLHttp');
+    }
+    xhr.onreadystatechange = function () {
+
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(2)
+            callback(xhr.responseText);
+        }
+    }
+
+    if (method == 'GET') {
+        var date = new Date(),
+            timer = date.getTime();
+        xhr.open('GET', url + '?' + data + '&timer' + timer, flag);
+        xhr.send()
+    } else if (method == 'POST') {
+        xhr.open('POST', url, flag);
+        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+        xhr.send(data);
+    }
+}
