64fba9907554d0f8a69c6dba9205c437466f09ff
张印 <550703617@qq.com>
2018-12-01 10:28:04 +0800

    mine.js增加了内容

--- mine.js
+++ mine.js
@@ -578,3 +578,21 @@ function ajax(method, url, callback, data, flag) {
         xhr.send(data);
     }
 }
+
+// 35.异步加载script
+function loadScript(url, callback) {
+    var oscript = document.createElement('script');
+    if (oscript.readyState) { // ie8及以下版本
+        oscript.onreadystatechange = function () {
+            if (oscript.readyState === 'complete' || oscript.readyState === 'loaded') {
+                callback();
+            }
+        }
+    } else {
+        oscript.onload = function () {
+            callback()
+        };
+    }
+    oscript.src = url;
+    document.body.appendChild(oscript);
+}
