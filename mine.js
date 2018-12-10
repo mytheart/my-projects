a44517771ae03686fbc468902c81a7b1df90dcfb
张印 <550703617@qq.com>
2018-12-10 17:30:31 +0800

    修改内容

--- mine.js
+++ mine.js
@@ -599,14 +599,14 @@ function loadScript(url, callback) {
 
 // 36.cookie管理
 var cookie = {
-    setCookie: function (name, value, time) {
+    set: function (name, value, time) {
         document.cookie = name + '=' + value + '; max-age=' + time;
         return this;
     },
-    removeCookie: function (name) {
+    remove: function (name) {
         return this.setCookie(name, '', -1);
     },
-    getCookie: function (name, callback) {
+    get: function (name, callback) {
         var allCookieArr = document.cookie.split('; ');
         for (var i = 0; i < allCookieArr.length; i++) {
             var itemCookieArr = allCookieArr[i].split('=');
